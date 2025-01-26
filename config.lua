2444aea405ff41ad439552b0a4079a7824d06c32
KingKayden <108900206+KingKayden1@users.noreply.github.com>
2025-01-26 01:34:29 -0600

    Initial Change

---
+++ config.lua
@@ -0,0 +1,12 @@
+-------------------
+-- CONFIG --
+-------------------
+Config = {}
+
+Config.Doctor = 0 -- Minimum Amount of EMS to work
+Config.Price = 100
+Config.ReviveTime = 10000  --in msec
+
+-- StarFall Edits
+Config.Notify = 'qb'  -- Options are ox or qb working on others
+
